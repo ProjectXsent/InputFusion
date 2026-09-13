ffbc3bb4399f7199e893bccb6606879eb88c3442
ProjectXsent <25782670+ProjectXsent@users.noreply.github.com>
2026-09-13 16:06:59 +0000

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -276,7 +276,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     switch (ul_reason_for_call)
     {
         case DLL_PROCESS_ATTACH: {
-            NativeInputBlock_Install();
+            //NativeInputBlock_Install();
             DisableThreadLibraryCalls(hModule);
             SDL_Init_Wait = CreateEvent(nullptr, TRUE, FALSE, nullptr);
             SDL_Quit_Wait = CreateEvent(nullptr, TRUE, FALSE, nullptr);
